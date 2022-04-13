a76d0510e0aae0a4498a7bbd9777302f463ace3a
markiep <purcellmarkie@gmail.com>
2022-04-13 08:34:43 -0700

    update

--- .github/install.sh
+++ .github/install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 # install all configs
+
+# for easy install, run:
+## curl "https://github.com/NYANLAUNCHER/.dotfiles/blob/master/.github/install.sh" > "$HOME/install.sh"
+## $HOME/install.sh
+
+
