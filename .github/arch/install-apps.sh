3d40d60387358006fe963a12379db3534245d606
markiep <purcellmarkie@gmail.com>
2022-04-13 07:29:18 -0700

    update

--- .github/arch/install-apps.sh
+++ .github/arch/install-apps.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-# Install apps specified with the apps var
+# Install apps|packages for Arch linux
+## setup.sh handles the base packages
 
-apps=""
+pacman -S neovim awesome xorg-xinit xorg-xmodmap xorg-xev xorg-setxkbmap xorg-xset rofi ripgrep zathura zathura-pdf-poppler \
+          picom boost boost-libs clang qutebrowser sl pamac lutris wine winetricks godot glxinfo yay emacs tree sxiv \
+          nvtop ufw trash-cli obs-studio sxhkd kitty blender krita gimp thunar python-pip python virtualbox snapd flameshot btop
+
+pamac build minecraft-launcher
+
+#"https://github.com/pystardust/ani-cli"
+#"https://ardour.org/building_linux.html"
+#"https://github.com/flightlessmango/MangoHud.git"
+#"https://docs.anbox.io/userguide/install.html"
+#"https://github.com/andreafrancia/trash-cli"
--- .github/arch/install-cfg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-# Install configs for Arch Linux
