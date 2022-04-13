9b54dd5018ff5bbeccd956ffb4f27cddcdc210c9
markiep <purcellmarkie@gmail.com>
2022-04-13 08:07:59 -0700

    update

--- .github/arch/setup.sh
+++ .github/arch/setup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 # My Arch install script
+
+update_user_dirs () {
+    source "$HOME/.config/user-dirs.dirs"
+    mkdir -p $XDG_DOCUMENTS_DIR $XDG_DOWNLOAD_DIR $XDG_DESKTOP_DIR $XDG_PICTURES_DIR $XDG_VIDEOS_DIR $XDG_MUSIC_DIR $XDG_TEMPLATES_DIR $XDG_PUBLICSHARE_DIR
+}
