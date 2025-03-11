acc0292f08c6a57b47c045d696faaa0e6d2239c0
markiep <purcellmarkie@proton.me>
2025-03-11 07:52:55 -0500

    update

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";  # You can choose stable or unstable branches
     flake-utils.url = "github:numtide/flake-utils";
-    neovim.url = "path:./.config/nvim";
-    yazi.url = "path:./.config/yazi";
+    neovim.url = "git+file://$HOME?dir=.config/neovim";
+    yazi.url = "git+file://$HOME?dir=.config/yazi";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let
