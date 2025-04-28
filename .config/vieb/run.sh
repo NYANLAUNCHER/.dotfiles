c5979866db437b83b476b4ac6fa35521fb156094
markiep <purcellmarkie@proton.me>
2025-04-28 07:57:13 -0500

    update

--- .config/vieb/run.sh
+++ .config/vieb/run.sh
@@ -3,6 +3,10 @@
 # --session="<path>" sets --datafolder=<dir>
 # If <path> doesn't start with "./" or "/" it is a path in $VIEB_SESSIONS_DIR
 # "-t"|"--temp" creates a temporary session 
+
+# if passed a directory it'll create a session folder inside it
+# if passed a file it'll try to open it
+
 . "$XDG_CONFIG_HOME/vieb/init.sh" # must be sourced
 args="$*" # gets parsed into vieb compatible args
 session_dir="$VIEB_DATAFOLDER" # the default session directory
@@ -19,5 +23,5 @@ args="$args --datafolder=\"${temp_session:-$session_dir}\""
 
 # vieb uses the original vieb cmd since it is in a sh script
 echo "vieb $args"
-#eval "vieb $args"
+eval "vieb $args"
 [ -d "$temp_session" ] && rm -rf "$temp_session" # remove the temporary session once vieb is closed
